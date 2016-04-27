442b76440001c4b1fa3350ef42cdf025df6d0fc9
Brad Spry <bradspry@users.noreply.github.com>
2016-04-27 15:50:31 -0400

    Eliminated watching of Drupal's public file directory
    Watching Drupal's public file directory sounded like a good idea; the goal was to detect web-originated ingests.   Ultimately, the method was not precise enough; I believe it created false positives.

--- script/ingestwatcher.sh
+++ script/ingestwatcher.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 drush_ready=$(ps aux 2>/dev/null |grep drush 2>/dev/null |wc -l)
 loadingdock_ready=$(/usr/bin/lsof /mnt/islandora-loadingdock | grep -e "[[:digit:]]\+[wu]\{1\}" |wc -l)
-drupalfiles_ready=$(/usr/bin/lsof /var/www/html/sites/default/files | grep -e "[[:digit:]]\+[wu]\{1\}" |wc -l)
-if (( $drush_ready == '0' || $drush_ready == '1' && $loadingdock_ready == '0' && $drupalfiles_ready == '0'))
+if (( $drush_ready == '0' || $drush_ready == '1' && $loadingdock_ready == '0'))
 then
 echo "#00FF00" > /var/www/html/blink/ingeststatus.txt
 else 
